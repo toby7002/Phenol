6160ee965e59f59bb3603049ab418bfeb8b50e41
Nguyen Minh Tho <minhthoj27@icloud.com>
2024-04-28 18:07:05 +0700

    Add language provider

---
+++ src/toby7002/Phenol/LanguageProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace toby7002\Phenol;
+
+class LanguageProvider
+{
+    /**
+     * @var string[] $translations
+     */
+    private array $translations = [];
+
+    public function loadTranslationsFromJSON(string $name, string $path): void {
+        $this->translations = json_decode(file_get_contents($path) ?: "", true);
+    }
+
+    public function getText(string $key, array $placeholders = []): string
+    {
+        if (!array_key_exists($key, $this->translations)) {
+            return $key;
+        }
+
+        $translation = $this->translations[$key];
+
+        foreach ($placeholders as $placeholderKey => $placeholderValue) {
+            $translation = str_replace("{{" . $placeholderKey . "}}", (string) $placeholderValue, $translation);
+        }
+
+        return $translation;
+    }
+}
